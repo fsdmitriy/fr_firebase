6eb1911489babe227bf382c439fa29b66f105162
fsdmitriy <dmitriy.fokasii@gmail.com>
2018-05-23 22:09:22 +0300

    added firebase event listener

--- fire_chat/app/src/main/java/com/firechat/fire_chat/MessageAdapter.java
+++ fire_chat/app/src/main/java/com/firechat/fire_chat/MessageAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import java.util.List;
 
 public class MessageAdapter extends ArrayAdapter<FriendlyMessage> {
+
     public MessageAdapter(Context context, int resource, List<FriendlyMessage> objects) {
         super(context, resource, objects);
     }
